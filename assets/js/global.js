bdf26d070c0f9cc6dff89da9edf2136ca4edac73
Mudassir Nawaz <mudassirnawazjavaid@gmail.com>
2025-01-04 21:42:58 +0500

    init commit

---
+++ assets/js/global.js
@@ -0,0 +1,23 @@
+!function(o, c) {
+    var n = c.documentElement
+      , t = " w-mod-";
+    n.className += t + "js",
+    ("ontouchstart"in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+}(window, document);
+
+const navButton = document.getElementById('nav-button');
+const nav = document.getElementById('navigation');
+
+navButton.addEventListener('click', () => {
+    if (nav.classList.contains('open')) {
+        closeMenu()
+    } else {
+        openMenu()
+    }
+});
+function openMenu() {
+    nav.classList.add('open');
+};
+function closeMenu() {
+    nav.classList.remove('open');
+};
